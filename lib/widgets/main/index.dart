53232db3b15bff3a8c18a579037ba8917de889f6
lumod97 <luiggigmd_97@outlook.com>
2023-08-23 21:01:20 -0500

    second

--- lib/widgets/main/index.dart
+++ lib/widgets/main/index.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -22,18 +24,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<int> numeros = [];
   final TextEditingController titleController = TextEditingController();
   final TextEditingController bodyController = TextEditingController();
   List<dynamic> data = [];
 
-  void navigateToDetails(Map<String, dynamic> item) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => DetailsScreen(item: item),
-      ),
-    );
-  }
+  // void navigateToDetails(Map<String, dynamic> item) {
+  //   Navigator.push(
+  //     context,
+  //     // MaterialPageRoute(
+  //     //   // builder: (context) => CustomBottomSheet(item: item),
+  //     // ),
+  //   );
+  // }
 
   Future<void> postData() async {
     var respuesta = '';
@@ -79,15 +82,6 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(labelText: 'Title'),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: bodyController,
-                decoration: InputDecoration(labelText: 'Body'),
-              ),
               SizedBox(height: 16),
               ElevatedButton(
                 onPressed: () {
@@ -105,7 +99,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         title: Text(data[index]['title']),
                         subtitle: Text(data[index]['body']),
                         onTap: () {
-                          navigateToDetails(
+                          _showModernBottomSheet(context,
                               data[index]); // Navegar a la pantalla de detalles
                         },
                       ),
@@ -118,5 +112,142 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  void _showModernBottomSheet(BuildContext context, Object item) {
+    if (item is Map<String, dynamic>) {
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+        ),
+        builder: (BuildContext context) {
+          String title = item['title'];
+          String image = item['image'];
+          String body = item['body'];
+          String price = item['price'];
+          List<String> cremas = item['cremas'].cast<String>();
+          numeros = List<int>.generate(cremas.length, (index) => 1);
+          // List<String> cremasList = (cremas as List).cast<String>();
+
+          return FractionallySizedBox(
+            heightFactor: 0.50,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                color: Colors.white,
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          title,
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'SanFranciscoBold'),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 1, bottom: 10),
+                        child: Text(
+                          'S/${price}',
+                          style: TextStyle(
+                              color: Colors.redAccent,
+                              fontFamily: 'SanFrancisco',
+                              fontSize: 20),
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(
+                            10.0), // Define el radio de las esquinas redondeadas
+                        child: Image(
+                          image: NetworkImage(image),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          'Descripcion',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Text(body),
+                      Padding(
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        child: Text(
+                          'Agrega cremas!',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 25),
+                        ),
+                      ),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: cremas.length,
+                        itemBuilder: (context, i) {
+                          return Row(
+                            children: [
+                              Text(
+                                cremas[i]
+                              ),
+                              OutlinedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (numeros[i] < 99) {
+                                      numeros[i]++;
+                                    }
+                                  });
+                                },
+                                style: OutlinedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(3),
+                                  side:
+                                      BorderSide(width: 1, color: Colors.blue),
+                                ),
+                                child: Icon(Icons.add,
+                                    size: 15, color: Colors.blue),
+                              ),
+                              SizedBox(width: 10),
+                              Text(numeros[i].toString()),
+                              SizedBox(width: 10),
+                              OutlinedButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (numeros[i] > 1) {
+                                      numeros[i]--;
+                                    }
+                                  });
+                                },
+                                style: OutlinedButton.styleFrom(
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(3),
+                                  side:
+                                      BorderSide(width: 1, color: Colors.blue),
+                                ),
+                                child: Icon(Icons.remove,
+                                    size: 15, color: Colors.blue),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
   }
 }
--- lib/widgets/main/views/details-item.dart
+++ lib/widgets/main/views/details-item.dart
@@ -13,14 +13,33 @@ class DetailsScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image(image: NetworkImage('https://cdn-icons-png.flaticon.com/512/857/857681.png')),
-            Text('Title: ${item['title']}'),
-            SizedBox(height: 8),
-            Text('Body: ${item['body']}'),
-            // Puedes mostrar más detalles aquí si los tienes en tu estructura de datos
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 16.0), // Margen a la derecha
+                  child: Image(
+                    image: NetworkImage(
+                        'https://cdn-icons-png.flaticon.com/512/857/857681.png'),
+                    width: 100, // Ancho deseado
+                    height: 100, // Alto deseado
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Title: ${item['title']}'),
+                  SizedBox(height: 8),
+                  Text('Body: ${item['body']}'),
+                  // Puedes mostrar más detalles aquí si los tienes en tu estructura de datos
+                ],
+              ),
+            ),
           ],
         ),
       ),
